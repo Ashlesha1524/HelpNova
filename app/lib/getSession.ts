d3f1ef5ee3347a4d7f2414055ac5f90c989c5a7a
Ashlesha1524 <ashleshamishra020@gmail.com>
2026-05-11 15:56:41 +0530

    Initial HelpNova auth setup

---
+++ app/lib/getSession.ts
@@ -0,0 +1,8 @@
+import { cookies } from "next/headers";
+
+export async function getSession(){
+    const session=await cookies()
+    const token=session.get("access_token")?.value
+    console.log(token)
+
+}
